edf0c3f6d46a4767620844ac60586f58c382ec85
davigueras <davigueras@gmail.com>
2020-06-29 18:47:05 +0200

    Add testing class.

---
+++ src/buchalka/masterclass/test.java
@@ -0,0 +1,31 @@
+package buchalka.masterclass;
+
+import java.math.BigDecimal;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		short myShortMinValue = Short.MIN_VALUE;
+		short myShortMaxValue = Short.MAX_VALUE;
+		
+		int myIntegerMinValue = Integer.MIN_VALUE;
+		int myIntegerMaxValue = Integer.MAX_VALUE;
+		
+		long myLongMinValue = Long.MIN_VALUE;
+		long myLongMaxValue = Long.MAX_VALUE;
+		
+
+		
+		float myFloatMinValue = Float.MIN_VALUE;
+		float myFloatMaxValue = Float.MAX_VALUE;
+		
+		System.out.println("Short - Min: " + myShortMinValue + " - Max: " + myShortMaxValue);
+		System.out.println("Integer - Min: " + myIntegerMinValue + " - Max: " + myIntegerMaxValue);
+		System.out.println("Long - Min: " + myLongMinValue + " - Max: " + myLongMaxValue);
+		System.out.println("Float - Min: " + myFloatMinValue + " - Max: " + myFloatMaxValue);
+			
+
+	}
+
+}
